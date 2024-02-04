58b7cf8a3b74d6fb4282f0653a5299f6d6e46512
Alex Batalov <radist2s@gmail.com>
2024-02-04 23:32:35 +0400

    refactor: rename cli options and reduce short options

--- packages/cli/src/bin.ts
+++ packages/cli/src/bin.ts
@@ -11,26 +11,26 @@ program
   )
   .argument('[input]', 'Input OpenAPI Schema file path, URL (json, yml)', null)
   .requiredOption(
-    '-o, --out-dir <path>',
+    '-o, --output-dir <path>',
     'Output directory for generated services'
   )
   .option(
-    '-g, --operation-generics-path <path>',
+    '--operation-generics-path <path>',
     'Path to operation generics file',
     '@radist2s/qraft/ServiceOperation'
   )
   .option(
-    '-t, --schema-types-path <path>', // todo::specify better param name to avoid confusion with real path
+    '--schema-types-path <path>', // todo::specify better param name to avoid confusion with real path
     'Path to schema types file (.d.ts)',
     './schema'
   )
   .option(
-    '-fh, --file-header <string>',
+    '--file-header <string>',
     'Header to be added to the generated file (eg: /* eslint-disable */)'
   )
   .option('-rm, --clean', 'Clean output directory before generating services')
   .option(
-    '-ps, --postfix-services <string>',
+    '--postfix-services <string>',
     'Postfix to be added to the generated service name (eg: Service)'
   )
   .action(async (input, args) => {
@@ -55,7 +55,7 @@ program
         schemaTypesPath: args.schemaTypesPath,
       },
       output: {
-        dir: args.outDir,
+        dir: args.outputDir,
         clean: args.clean,
         fileHeader: args.fileHeader ?? fileHeader,
         postfixServices: args.postfixServices,
