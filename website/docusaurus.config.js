e017cd87f52039f32f93a9a91dacea7601d100fb
Alex Batalov <radist2s@gmail.com>
2024-03-18 07:05:55 +0400

    fix: documentation Website base url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://openapi-qraft.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/openapi-qraft/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
