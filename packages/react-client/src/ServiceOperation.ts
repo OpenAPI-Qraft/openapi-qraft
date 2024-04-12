2853879739e53a9853bfb5c8eb85c68dece0289f
Alex Batalov <radist2s@gmail.com>
2024-04-13 03:01:13 +0400

    feature: add support of not exact query key in Query Filters

--- packages/react-client/src/ServiceOperation.ts
+++ packages/react-client/src/ServiceOperation.ts
@@ -412,41 +412,97 @@ interface QueryFiltersBase<
     : TInfinite extends false
       ? QueryFilterRegularPredicate<TSchema, TData, TParams, TError>
       : (query: string) => boolean;
+
   /**
    * Filter to active queries, inactive queries or all queries
    */
   type?: QueryTypeFilter;
-  /**
-   * Match query key exactly
-   */
-  exact?: boolean;
+
   /**
    * Include or exclude stale queries
    */
   stale?: boolean;
+
   /**
    * Include queries matching their fetchStatus
    */
   fetchStatus?: FetchStatus;
 }
 
-interface QueryFiltersByQueryKey<
+interface QueryFiltersByQueryKeyBase<
   TSchema extends { url: string; method: string },
   TData,
   TInfinite extends boolean,
   TParams = {},
   TError = DefaultError,
 > extends QueryFiltersBase<TSchema, TData, TInfinite, TParams, TError> {
-  /**
-   * Include queries matching this query key
-   */
-  queryKey?: ServiceOperationBaseQueryKey<TSchema, TInfinite, TParams>;
-
   infinite?: never;
   parameters?: never;
 }
 
-interface QueryFiltersByParameters<
+interface QueryFiltersByExactQueryKey<
+  TSchema extends { url: string; method: string },
+  TData,
+  TInfinite extends boolean,
+  TParams = {},
+  TError = DefaultError,
+> extends QueryFiltersByQueryKeyBase<
+    TSchema,
+    TData,
+    TInfinite,
+    TParams,
+    TError
+  > {
+  /**
+   * Include queries matching this query key
+   */
+  queryKey: ServiceOperationBaseQueryKey<TSchema, TInfinite, TParams>;
+
+  /**
+   * Match query key exactly
+   */
+  exact: true;
+}
+
+interface QueryFiltersWeakExactQueryKey<
+  TSchema extends { url: string; method: string },
+  TData,
+  TInfinite extends boolean,
+  TParams = {},
+  TError = DefaultError,
+> extends QueryFiltersByQueryKeyBase<
+    TSchema,
+    TData,
+    TInfinite,
+    TParams,
+    TError
+  > {
+  /**
+   * Include queries matching this query key
+   */
+  queryKey?: ServiceOperationBaseQueryKey<
+    TSchema,
+    TInfinite,
+    PartialParameters<TParams>
+  >;
+
+  /**
+   * Match query key exactly
+   */
+  exact?: false;
+}
+
+type QueryFiltersByQueryKey<
+  TSchema extends { url: string; method: string },
+  TData,
+  TInfinite extends boolean,
+  TParams = {},
+  TError = DefaultError,
+> =
+  | QueryFiltersByExactQueryKey<TSchema, TData, TInfinite, TParams, TError>
+  | QueryFiltersWeakExactQueryKey<TSchema, TData, TInfinite, TParams, TError>;
+
+interface QueryFiltersByParametersBase<
   TSchema extends { url: string; method: string },
   TData,
   TInfinite extends boolean,
@@ -458,13 +514,66 @@ interface QueryFiltersByParameters<
    */
   infinite?: TInfinite;
 
+  queryKey?: never;
+}
+
+interface QueryFiltersByExactParameters<
+  TSchema extends { url: string; method: string },
+  TData,
+  TInfinite extends boolean,
+  TParams = {},
+  TError = DefaultError,
+> extends QueryFiltersByParametersBase<
+    TSchema,
+    TData,
+    TInfinite,
+    TParams,
+    TError
+  > {
+  /**
+   * Match query key exactly
+   */
+  exact: true;
+
   /**
    * Include queries matching parameters
    */
-  parameters?: TParams;
-
-  queryKey?: never;
+  parameters: TParams;
 }
+
+interface QueryFiltersByWeakParameters<
+  TSchema extends { url: string; method: string },
+  TData,
+  TInfinite extends boolean,
+  TParams = {},
+  TError = DefaultError,
+> extends QueryFiltersByParametersBase<
+    TSchema,
+    TData,
+    TInfinite,
+    TParams,
+    TError
+  > {
+  /**
+   * Match query key exactly
+   */
+  exact?: false;
+
+  /**
+   * Include queries matching parameters
+   */
+  parameters?: PartialParameters<TParams>;
+}
+
+type QueryFiltersByParameters<
+  TSchema extends { url: string; method: string },
+  TData,
+  TInfinite extends boolean,
+  TParams = {},
+  TError = DefaultError,
+> =
+  | QueryFiltersByExactParameters<TSchema, TData, TInfinite, TParams, TError>
+  | QueryFiltersByWeakParameters<TSchema, TData, TInfinite, TParams, TError>;
 
 interface QueryFilterInfinitePredicate<
   TSchema extends { url: string; method: string },
