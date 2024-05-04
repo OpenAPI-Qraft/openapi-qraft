022002b1a71c187a8e72aa01e040383f63cf73b3
Alex Batalov <radist2s@gmail.com>
2024-05-04 21:52:05 +0400

    refactor(openapi-typescript-plugin): simplify bin logic

--- packages/openapi-typescript-plugin/src/bin.ts
+++ packages/openapi-typescript-plugin/src/bin.ts
@@ -3,17 +3,16 @@ import { createConfig } from '@redocly/openapi-core';
 
 import { program } from 'commander';
 import fs from 'node:fs';
-import { Readable } from 'node:stream';
+import type { Readable, Writable } from 'node:stream';
 import openapiTS, {
   astToString,
   c,
   COMMENT_HEADER,
   formatTime,
   OpenAPI3,
+  OpenAPITSOptions,
 } from 'openapi-typescript';
 import ts from 'typescript';
-
-const timeStart = performance.now();
 
 program
   .description(
@@ -41,102 +40,145 @@ program
   .option('--alphabetize', 'Sort object keys alphabetically')
   .option('--exclude-deprecated', 'Exclude deprecated types')
   .action(async (input, args) => {
-    const CWD = new URL(`file://${process.cwd()}/`);
+    const timeStart = performance.now();
+
+    const cwd = new URL(`file://${process.cwd()}/`);
     const output = (args.output as string) || process.stdout;
 
-    async function generateAndWriteSchema(
-      schema: string | URL | OpenAPI3 | Readable | Buffer
-    ) {
-      const result = `${COMMENT_HEADER}${astToString(
-        await openapiTS(schema, {
-          transform(schemaObject) {
-            if (schemaObject.format === 'binary') {
-              return {
-                schema: schemaObject.nullable
-                  ? ts.factory.createUnionTypeNode([
-                      ts.factory.createTypeReferenceNode(
-                        ts.factory.createIdentifier('Blob'),
-                        undefined
-                      ),
-                      ts.factory.createLiteralTypeNode(ts.factory.createNull()),
-                    ])
-                  : ts.factory.createTypeReferenceNode(
-                      ts.factory.createIdentifier('Blob'),
-                      undefined
-                    ),
-                // questionToken will be inferred by `openapiTS`, if true, it will force `?` for parameter
-                questionToken: false,
-              };
-            }
-          },
-          additionalProperties: args.additionalProperties,
-          alphabetize: args.alphabetize,
-          arrayLength: args.arrayLength,
-          // contentNever: args.contentNever, // todo::add `contentNever` in new version
-          defaultNonNullable: args.defaultNonNullable,
-          emptyObjectsUnknown: args.emptyObjectsUnknown,
-          enum: args.enum,
-          excludeDeprecated: args.excludeDeprecated,
-          exportType: args.exportType,
-          immutable: args.immutable,
-          pathParamsAsTypes: args.pathParamsAsTypes,
-          redocly: await createConfig({}, { extends: ['minimal'] }),
-          silent: typeof output !== 'string',
-        })
-      )}`;
+    await generateAndWriteSchema(handleSchemaInput(input, cwd), {
+      output,
+      args,
+      cwd,
+    });
 
-      if (typeof output === 'string') {
-        const outFile = new URL(output, CWD);
-        fs.mkdirSync(new URL('.', outFile), { recursive: true });
-        fs.writeFileSync(outFile, result, 'utf8');
-      } else {
-        // if stdout, (still) don’t log anything to console!
-        output.write(result);
-      }
-
-      if (typeof output === 'string') {
-        done(input || 'stdin', output, performance.now() - timeStart);
-      }
-    }
-
-    if (!input) {
-      if (process.stdin.isTTY) {
-        console.error(
-          c.red(
-            'Input file not found or stdin is empty. Please specify `--input` option or pipe OpenAPI Schema to stdin.'
-          )
-        );
-
-        process.exit(1);
-      }
-
-      await generateAndWriteSchema(process.stdin);
-    }
-    // handle single file
-    else {
-      // throw error on glob
-      if (input.includes('*')) {
-        errorAndExit(
-          'Globbing is not supported. Please specify a single file or URL.'
-        );
-      }
-
-      await generateAndWriteSchema(new URL(input, CWD));
+    if (output && typeof output === 'string') {
+      // if stdout, (still) don’t log anything to console!
+      console.log(
+        `🚀 ${c.green(`${input || 'stdin'} → ${c.bold(output)}`)} ${c.dim(
+          `[${formatTime(performance.now() - timeStart)}]`
+        )}`
+      );
     }
   });
 
-function done(input: string, output: string, time: number) {
-  // final console output
-  console.log(
-    `🚀 ${c.green(`${input} → ${c.bold(output)}`)} ${c.dim(
-      `[${formatTime(time)}]`
-    )}`
-  );
+async function generateAndWriteSchema(
+  schema: string | URL | OpenAPI3 | Readable | Buffer,
+  {
+    cwd,
+    output,
+    args,
+  }: {
+    cwd: URL;
+    output: string | Writable;
+    args: Pick<
+      OpenAPITSOptions,
+      | 'additionalProperties'
+      | 'alphabetize'
+      | 'arrayLength'
+      | 'defaultNonNullable'
+      | 'emptyObjectsUnknown'
+      | 'enum'
+      | 'excludeDeprecated'
+      | 'exportType'
+      | 'immutable'
+      | 'pathParamsAsTypes'
+    >;
+  }
+) {
+  const result = `${COMMENT_HEADER}${astToString(
+    await openapiTS(schema, {
+      transform(schemaObject) {
+        if (schemaObject.format === 'binary') {
+          return {
+            schema: schemaObject.nullable
+              ? ts.factory.createUnionTypeNode([
+                  ts.factory.createTypeReferenceNode(
+                    ts.factory.createIdentifier('Blob'),
+                    undefined
+                  ),
+                  ts.factory.createLiteralTypeNode(ts.factory.createNull()),
+                ])
+              : ts.factory.createTypeReferenceNode(
+                  ts.factory.createIdentifier('Blob'),
+                  undefined
+                ),
+            // questionToken will be inferred by `openapiTS`, if true, it will force `?` for parameter
+            questionToken: false,
+          };
+        }
+      },
+      additionalProperties: args.additionalProperties,
+      alphabetize: args.alphabetize,
+      arrayLength: args.arrayLength,
+      // contentNever: args.contentNever, // todo::add `contentNever` in new version
+      defaultNonNullable: args.defaultNonNullable,
+      emptyObjectsUnknown: args.emptyObjectsUnknown,
+      enum: args.enum,
+      excludeDeprecated: args.excludeDeprecated,
+      exportType: args.exportType,
+      immutable: args.immutable,
+      pathParamsAsTypes: args.pathParamsAsTypes,
+      redocly: await createConfig({}, { extends: ['minimal'] }),
+      silent: typeof output !== 'string',
+    })
+  )}`;
+
+  if (typeof output === 'string') {
+    const outFile = new URL(output, cwd);
+    fs.mkdirSync(new URL('.', outFile), { recursive: true });
+    fs.writeFileSync(outFile, result, 'utf8');
+  } else {
+    // if stdout, (still) don’t log anything to console!
+    output.write(result);
+  }
 }
 
-function errorAndExit(message: string) {
-  console.error(c.red(` ✘  ${message}`));
-  throw new Error(message);
+/**
+ * Validates Schema's `input` and return `Readable` stream or `URL`
+ * Exit process if input is invalid
+ */
+function handleSchemaInput(input: string | undefined, cwd: URL) {
+  if (!input) {
+    /**
+     * Handle `stdin`, if no input file is provided
+     * @example
+     * ```bash
+     * cat schema.json | script.js
+     * ```
+     */
+    if (process.stdin.isTTY) {
+      console.error(
+        c.red(
+          ' ✘  Input file not found or stdin is empty. Please specify `--input` option or pipe OpenAPI Schema to stdin.'
+        )
+      );
+
+      process.exit(1);
+    }
+
+    return process.stdin;
+  }
+
+  /**
+   * Handle single file
+   * @example
+   * ```bash
+   * script.js schema.json
+   * ```
+   */
+
+  // throw error on glob
+  if (input.includes('*')) {
+    console.error(
+      c.red(
+        ' ✘  Globbing is not supported. Please specify a single file or URL.'
+      )
+    );
+
+    process.exit(1);
+  }
+
+  return new URL(input, cwd);
 }
 
 program.parse(process.argv);
