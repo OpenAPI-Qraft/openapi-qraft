5a934d69bf58a73df07678577736df26420b8b62
Alex Batalov <radist2s@gmail.com>
2024-05-11 01:54:26 +0400

    feature(openapi-typescript-plugin): add tests

--- packages/openapi-typescript-plugin/src/plugin.test.ts
+++ packages/openapi-typescript-plugin/src/plugin.test.ts
@@ -1,6 +1,62 @@
-import { describe, it, expect } from 'vitest';
+import '@openapi-qraft/plugin/lib/vitestFsMock';
+
+import fs from 'node:fs';
+import { createRequire } from 'node:module';
+import { describe, expect, it, test } from 'vitest';
 
 import { openapiTypesFileNameOptionParser } from './plugin.js';
+
+describe('openapi-typescript types generation', () => {
+  const openAPIDocumentFixturePath = createRequire(process.cwd()).resolve(
+    '@openapi-qraft/test-fixtures/openapi.json'
+  );
+
+  test('no extra options', async () => {
+    const { QraftCommand } = await import(
+      '@openapi-qraft/plugin/lib/QraftCommand'
+    );
+    const { plugin } = await import('./plugin.js');
+    const command = new QraftCommand();
+    plugin.setupCommand(command);
+
+    await command.parseAsync([
+      'dummy-node',
+      'dummy-qraft-bin',
+      openAPIDocumentFixturePath,
+      '-o',
+      '/mock-fs',
+    ]);
+
+    expect(fs.readFileSync('/mock-fs/schema.ts', 'utf-8')).toMatchFileSnapshot(
+      './__snapshots__/no-extra-options.ts.snapshot.ts'
+    );
+  });
+
+  test('with extra options', async () => {
+    const { QraftCommand } = await import(
+      '@openapi-qraft/plugin/lib/QraftCommand'
+    );
+    const { plugin } = await import('./plugin.js');
+    const command = new QraftCommand();
+    plugin.setupCommand(command);
+
+    await command.parseAsync([
+      'dummy-node',
+      'dummy-qraft-bin',
+      openAPIDocumentFixturePath,
+      '-o',
+      '/mock-fs',
+      '--openapi-types-file-name',
+      'openapi.ts',
+      '--filter-services',
+      '/files/**',
+    ]);
+
+    expect(fs.readFileSync('/mock-fs/openapi.ts', 'utf-8')).toMatchFileSnapshot(
+      './__snapshots__/with-extra-options.ts.snapshot.ts'
+    );
+  });
+});
 
 describe('openapiTypesFileNameOptionParser', () => {
   it('should return the input value if valid', () => {
