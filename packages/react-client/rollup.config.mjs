25ebc31f90db35b1950214b961f608151e1f30b1
Alex Batalov <radist2s@gmail.com>
2024-06-30 20:50:25 +0400

    chore(react-client): update shared `rollup.config.ts`
    * updated react-client `rollup.config.mjs`

--- packages/react-client/rollup.config.mjs
+++ packages/react-client/rollup.config.mjs
@@ -2,7 +2,15 @@ import { rollupConfig } from '@openapi-qraft/rollup-config';
 
 import packageJson from './package.json' assert { type: 'json' };
 
-export default rollupConfig({
-  module: packageJson['exports']['.']['import'],
-  main: packageJson['exports']['.']['require'],
-});
+const moduleDist = {
+  import: packageJson['exports']['.']['import'],
+  require: packageJson['exports']['.']['require'],
+};
+
+const config = [
+  rollupConfig(moduleDist, { input: 'src/index.ts' }),
+  rollupConfig(moduleDist, { input: 'src/callbacks/index.ts' }),
+  rollupConfig(moduleDist, { input: 'src/Unstable_QraftSecureRequestFn.ts' }),
+];
+
+export default config;
