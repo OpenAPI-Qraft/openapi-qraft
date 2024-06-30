6fcf9b95253eac279292c9ca64a111b75ebf0f00
Alex Batalov <radist2s@gmail.com>
2024-06-30 23:00:05 +0400

    chore(e2e): add extra tests to `typescript-commonjs-node`

--- e2e/projects/typescript-commonjs-node/src/index.ts
+++ e2e/projects/typescript-commonjs-node/src/index.ts
@@ -1,8 +1,55 @@
+import * as callbacks from '@openapi-qraft/react/callbacks/index';
+import { useMutation } from '@openapi-qraft/react/callbacks/useMutation';
+import { useQuery } from '@openapi-qraft/react/callbacks/useQuery';
+import {
+  QraftSecureRequestFn,
+  createSecureRequestFn,
+} from '@openapi-qraft/react/Unstable_QraftSecureRequestFn';
+
 import { createAPIClient as createAPIClientMjs } from './api';
 
 if (typeof createAPIClientMjs !== 'undefined') {
   console.log('Client is imported successfully from esm project.');
 } else {
   console.error('Client is not imported from esm project.');
+  process.exit(1);
+}
+
+if (typeof callbacks !== 'undefined') {
+  console.log('Callbacks are imported successfully from esm project.');
+} else {
+  console.error('Callbacks are not imported from esm project.');
+  process.exit(1);
+}
+
+if (typeof useMutation !== 'undefined') {
+  console.log('useMutation is imported successfully from esm project.');
+} else {
+  console.error('useMutation is not imported from esm project.');
+  process.exit(1);
+}
+
+if (typeof useQuery !== 'undefined') {
+  console.log('useQuery is imported successfully from esm project.');
+} else {
+  console.error('useQuery is not imported from esm project.');
+  process.exit(1);
+}
+
+if (typeof createSecureRequestFn !== 'undefined') {
+  console.log(
+    'createSecureRequestFn is imported successfully from esm project.'
+  );
+} else {
+  console.error('createSecureRequestFn is not imported from esm project.');
+  process.exit(1);
+}
+
+if (typeof QraftSecureRequestFn !== 'undefined') {
+  console.log(
+    'QraftSecureRequestFn is imported successfully from esm project.'
+  );
+} else {
+  console.error('QraftSecureRequestFn is not imported from esm project.');
   process.exit(1);
 }
