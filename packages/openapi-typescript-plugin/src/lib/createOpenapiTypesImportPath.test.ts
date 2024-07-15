e1ff83d101f50def7912655c6d7b06af14f3a935
Alex Batalov <radist2s@gmail.com>
2024-07-16 03:06:26 +0400

    fix: `createOpenapiTypesImportPath()` with according tests

--- packages/openapi-typescript-plugin/src/lib/createOpenapiTypesImportPath.test.ts
+++ packages/openapi-typescript-plugin/src/lib/createOpenapiTypesImportPath.test.ts
@@ -3,25 +3,25 @@ import { describe, expect, it } from 'vitest';
 import { createOpenapiTypesImportPath } from './createOpenapiTypesImportPath.js';
 
 describe('createOpenapiTypesImportPath', () => {
-  it('should create import path with explicit extensions from ".d.ts"', () => {
+  it('should create import path with explicit extensions "true" and ".d.ts"', () => {
     expect(createOpenapiTypesImportPath('schema.d.ts', true)).toBe(
-      '../schema.js'
+      '../schema.d.ts'
     );
   });
 
-  it('should create import path with explicit extensions from ".d.ts"', () => {
+  it('should create import path with explicit extensions "false" and ".d.ts"', () => {
     expect(createOpenapiTypesImportPath('schema.d.ts', false)).toBe(
       '../schema.d.ts'
     );
   });
 
-  it('should create import path without explicit extensions from ".ts"', () => {
+  it('should create import path with explicit extensions "false" from ".ts"', () => {
     expect(createOpenapiTypesImportPath('schema.ts', false)).toBe(
       '../schema.ts'
     );
   });
 
-  it('should create import path with explicit extensions from ".ts"', () => {
+  it('should create import path with explicit extensions "true" from ".ts"', () => {
     expect(createOpenapiTypesImportPath('schema.ts', true)).toBe(
       '../schema.js'
     );
