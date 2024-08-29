2bc70405df94da4d8f57f33e8e817283249d87ec
Alex Batalov <radist2s@gmail.com>
2024-08-29 04:30:55 +0400

    refactor(tanstack-query-react-plugin): move `maybeResolveImport(...)` into separate file

--- packages/tanstack-query-react-plugin/src/ts-factory/getIndexFactory.ts
+++ packages/tanstack-query-react-plugin/src/ts-factory/getIndexFactory.ts
@@ -1,5 +1,6 @@
 import type { ServiceImportsFactoryOptions } from './getServiceFactory.js';
 import ts from 'typescript';
+import { maybeResolveImport } from '../lib/maybeResolveImport.js';
 
 export const getIndexFactory = ({
   openapiTypesImportPath,
@@ -71,23 +72,3 @@ export const getIndexFactory = ({
     ),
   ];
 };
-
-/**
- * Prepend the import path if it's a relative path.
- * Normally,`openapiTypesImportPath` is a relative path to the services' directory.
- */
-export function maybeResolveImport<T extends string | undefined>({
-  openapiTypesImportPath,
-  servicesDirName,
-}: {
-  openapiTypesImportPath: T;
-  servicesDirName: string;
-}) {
-  if (openapiTypesImportPath?.startsWith('../')) {
-    return `./${openapiTypesImportPath.slice(3)}`;
-  } else if (openapiTypesImportPath?.startsWith('./')) {
-    return `./${servicesDirName}/${openapiTypesImportPath.slice(2)}`;
-  }
-
-  return openapiTypesImportPath;
-}
