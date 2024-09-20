ca073837b9563e9190f332431b16aab2a8f6e1e7
Alex Batalov <radist2s@gmail.com>
2024-09-21 01:45:17 +0400

    feat(plugin)!: customize operation names with `--service-name-base` option
    BREAKING CHANGES:
    --service-name-base option affects the structure of generated names
    
    Examples:
    --service-name-base=endpoint[0]: POST /api/v1/users/{id} → postApiV1UsersId
    --service-name-base=tags: POST /api/v1/users/{id}/{key} → postApiV1UsersIdKey
    --service-name-base=endpoint[2]: POST /api/v1/users/{id}/{key} → postUsersIdKey

--- packages/plugin/src/lib/open-api/getOperationName.ts
+++ packages/plugin/src/lib/open-api/getOperationName.ts
@@ -1,4 +1,7 @@
-import camelCase from 'camelcase';
+import type { ServiceBaseNameByEndpointOption } from './getServiceNamesByOperationEndpoint.js';
+import type { ServiceBaseName } from './getServices.js';
+import camelcase from 'camelcase';
+import { getEndpointPartIndex } from './getServiceNamesByOperationEndpoint.js';
 
 /**
  * Convert the input value to a correct operation (method) classname.
@@ -8,22 +11,39 @@ import camelCase from 'camelcase';
 export const getOperationName = (
   url: string,
   method: string,
-  operationId: string | undefined
+  operationId: string | undefined,
+  serviceNameBase: ServiceBaseName
 ): string => {
   if (operationId) return getOperationIdName(operationId);
 
-  const urlWithoutPlaceholders = url
+  const urlWithoutPlaceholders = (
+    serviceNameBase === 'tags'
+      ? url
+      : reduceUrlByEndpointPartIndex(url, serviceNameBase)
+  )
     .replace(/{(.*?)}/g, '$1')
     .replace(/\//g, '-');
 
-  return camelCase(`${method}-${urlWithoutPlaceholders}`);
+  return camelcase(`${method}-${urlWithoutPlaceholders}`);
 };
 
 export const getOperationIdName = (operationId: string): string => {
-  return camelCase(
+  return camelcase(
     operationId
       .replace(/^[^a-zA-Z]+/g, '')
       .replace(/[^\w-]+/g, '-')
       .trim()
   );
+};
+
+export const reduceUrlByEndpointPartIndex = (
+  url: string,
+  serviceNameBase: ServiceBaseNameByEndpointOption
+) => {
+  const reducedPath = (url.startsWith('/') ? url.slice(1) : url)
+    .split('/')
+    .slice(getEndpointPartIndex(serviceNameBase))
+    .join('/');
+
+  return url.startsWith('/') ? `/${reducedPath}` : reducedPath;
 };
