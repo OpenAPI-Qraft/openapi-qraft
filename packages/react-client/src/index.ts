42f15042aaa1c8926034e4b33d2151010e067d6c
Alex Batalov <radist2s@gmail.com>
2025-04-12 22:09:46 +0400

    feat: allow creation API client without generic arguments

--- packages/react-client/src/index.ts
+++ packages/react-client/src/index.ts
@@ -5,6 +5,7 @@ export {
   type CreateAPIBasicClientOptions,
   type CreateAPIQueryClientOptions,
   type APIQueryClientServices,
+  type APIDefaultQueryClientServices,
   type APIBasicQueryClientServices,
   type APIBasicClientServices,
   type APIUtilityClientServices,
