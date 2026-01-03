8504879fd3681a8aeac58c413788f1a397e5b0dc
Alex Batalov <radist2s@gmail.com>
2026-01-04 03:00:25 +0400

    feat: enhance ParametersWrapper type to include TData and TError generics

--- packages/react-client/src/tests/fixtures/type-overrides/parameters-wrapper.ts
+++ packages/react-client/src/tests/fixtures/type-overrides/parameters-wrapper.ts
@@ -1,4 +1,6 @@
 export type ParametersWrapper<
   TSchema extends { method: string; url: string },
   TOperation extends { parameters: Record<string, any> },
+  TData,
+  TError,
 > = TOperation['parameters'];
