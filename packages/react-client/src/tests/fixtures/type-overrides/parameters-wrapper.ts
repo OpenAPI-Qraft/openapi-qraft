6068d0d1d526fee5e2c8ceecc4b2d66c66b6b17c
Alex Batalov <radist2s@gmail.com>
2026-01-04 03:49:21 +0400

    chore: add eslint ignore rule to the parameters-wrapper.ts

--- packages/react-client/src/tests/fixtures/type-overrides/parameters-wrapper.ts
+++ packages/react-client/src/tests/fixtures/type-overrides/parameters-wrapper.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 export type ParametersWrapper<
   TSchema extends { method: string; url: string },
   TOperation extends { parameters: Record<string, any> },
