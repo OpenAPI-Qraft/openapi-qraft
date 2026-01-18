613e71680fb199dd6cd13130b5dfd3f7d25596c8
Aleksandr Batalov <radist2s@gmail.com>
2026-01-18 20:29:57 +0400

    chore(tanstack-query-react-plugin): update snapshots

--- packages/tanstack-query-react-plugin/src/__snapshots__/create-api-client-fn/all-createAPIClient.ts.snapshot.ts
+++ packages/tanstack-query-react-plugin/src/__snapshots__/create-api-client-fn/all-createAPIClient.ts.snapshot.ts
@@ -13,12 +13,14 @@ export function createAPIClient(options: Context<CreateAPIQueryClientOptions>): 
 export function createAPIClient(options: CreateAPIBasicQueryClientOptions): APIBasicQueryClientServices<Services, AllCallbacks>;
 export function createAPIClient(options: CreateAPIBasicClientOptions): APIBasicClientServices<Services, AllCallbacks>;
 export function createAPIClient(): APIUtilityClientServices<Services, AllCallbacks>;
-export function createAPIClient(options?: CreateAPIClientOptions | Context<CreateAPIQueryClientOptions>): APIDefaultQueryClientServices<Services> | APIQueryClientHookServices<Services, AllCallbacks> | APIBasicQueryClientServices<Services, AllCallbacks> | APIBasicClientServices<Services, AllCallbacks> | APIUtilityClientServices<Services, AllCallbacks> {
+export function createAPIClient(options?: CreateAPIClientOptions | Context<CreateAPIQueryClientOptions>): APIDefaultQueryClientServices<Services> | APIBasicQueryClientServices<Services, AllCallbacks> | APIBasicClientServices<Services, AllCallbacks> | APIUtilityClientServices<Services, AllCallbacks> {
     if (!options)
         return qraftAPIClient(services, allCallbacks);
     if ("requestFn" in options)
         return qraftAPIClient(services, allCallbacks, options);
     if ("queryClient" in options)
+        return qraftAPIClient(services, allCallbacks, options);
+    if ("Provider" in options)
         return qraftAPIClient(services, allCallbacks, options);
     return qraftAPIClient(services, allCallbacks);
 }
