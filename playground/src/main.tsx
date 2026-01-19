36bde361978957dc5344d4d549e073ff833f779f
Aleksandr Batalov <radist2s@gmail.com>
2026-01-19 10:52:06 +0400

    chore(playground): add msw-auto-mock for the dev env

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function enableMocking() {
+  if (import.meta.env.DEV) {
+    const { worker } = await import('./mocks/browser.js');
+    return worker.start({
+      onUnhandledRequest: 'bypass',
+    });
+  }
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
