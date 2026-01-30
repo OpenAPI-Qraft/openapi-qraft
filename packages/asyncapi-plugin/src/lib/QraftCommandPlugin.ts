2d8abda09b80981185952685ca953428fcc9e8d4
Aleksandr Batalov <radist2s@gmail.com>
2026-01-31 02:28:25 +0400

    feat: add @qraft/asyncapi-plugin package

---
+++ packages/asyncapi-plugin/src/lib/QraftCommandPlugin.ts
@@ -0,0 +1,5 @@
+import { QraftCommandPlugin as QraftCommandPluginBase } from '@qraft/plugin';
+import { QraftCommand } from './QraftCommand.js';
+
+export interface QraftCommandPlugin
+  extends QraftCommandPluginBase<QraftCommand> {}
