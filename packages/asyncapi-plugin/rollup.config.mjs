5fb88e4445c4b45ceff411fdf514ff048609c488
Aleksandr Batalov <radist2s@gmail.com>
2026-02-02 16:30:34 +0400

    fixup! feat(asyncapi-plugin): bundle `@asyncapi/parser`

--- packages/asyncapi-plugin/rollup.config.mjs
+++ packages/asyncapi-plugin/rollup.config.mjs
@@ -1,15 +1,21 @@
 import { rollupConfig } from '@openapi-qraft/rollup-config';
+import renameNodeModules from 'rollup-plugin-rename-node-modules';
 import packageJson from './package.json' with { type: 'json' };
 
+const baseConfig = rollupConfig(
+  { import: packageJson['exports']['.']['import'] },
+  {
+    input: 'src/index.ts',
+    externalDependencies: Object.keys(packageJson.dependencies),
+    esmOnly: true,
+  }
+);
+
 const config = [
-  rollupConfig(
-    { import: packageJson['exports']['.']['import'] },
-    {
-      input: 'src/index.ts',
-      externalDependencies: Object.keys(packageJson.dependencies),
-      esmOnly: true,
-    }
-  ),
+  {
+    ...baseConfig,
+    plugins: [...baseConfig.plugins, renameNodeModules('_vendor')],
+  },
 ];
 
 export default config;
