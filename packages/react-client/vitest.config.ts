01528c67b8576c734ac96b898a574c91f9e45df7
Aleksandr Batalov <radist2s@gmail.com>
2026-04-02 20:21:20 +0400

    chore: exclude 'dist/**' from coverage in vitest configuration

--- packages/react-client/vitest.config.ts
+++ packages/react-client/vitest.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [react()],
   test: {
+    exclude: ['./dist/**'],
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/tests/setupTests.ts',
