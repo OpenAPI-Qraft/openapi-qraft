6c6ada096aefc9313c01bbff4f6092a14db4a817
Aleksandr Batalov <radist2s@gmail.com>
2026-04-02 23:12:29 +0400

    feat: add tests for applyRootSecurityToServices function and improve response handling

--- packages/openapi-plugin/src/lib/open-api/OpenAPISchemaType.ts
+++ packages/openapi-plugin/src/lib/open-api/OpenAPISchemaType.ts
@@ -24,7 +24,7 @@ export type OpenAPISchemaType = {
           required?: boolean;
         };
         responses: {
-          [statusCode in number | 'default']: {
+          [statusCode in number | 'default']?: {
             $ref?: string;
             description?: string;
             content?: {
