c9905a6936b3d9dcc1070564e7c667d5eaf83ab2
Alex Batalov <radist2s@gmail.com>
2026-05-09 00:44:28 +0400

    chore: add tree shaking plugin in playground

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,8 +1,18 @@
+import { qraftTreeShakeVite } from '@openapi-qraft/tree-shaking-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
+    qraftTreeShakeVite({
+      createAPIClientFn: [
+        {
+          name: 'createPlaygroundAPIClient',
+          module: './api',
+          context: 'PlaygroundAPIClientContext',
+        },
+      ],
+    }),
     react({
       babel: {
         plugins: [
