195b2e540ee58f2d5ee26b72306701efadef1dcf
Alex Batalov <radist2s@gmail.com>
2026-06-16 23:07:47 +0400

    fix: import process in tree shake bin wrapper

--- packages/tree-shaking-plugin/bin.mjs
+++ packages/tree-shaking-plugin/bin.mjs
@@ -5,6 +5,7 @@
  * Workaround to allow `rimraf dist/` on rebuilds and keep `bin` executable
  * without a need `yarn install`.
  */
+import process from 'node:process';
 import { main } from './dist/esm/bin.js';
 
 const exitCode = await main(process.argv);
